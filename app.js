93d8da107dd35fc65d9facafdaaa418d29c38542
Shivam Shukla <107310556+shivamshukla13jun@users.noreply.github.com>
2023-01-27 14:40:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,13 @@ const Port=process.env.PORT || 4000
 app.listen(Port,()=>{
     console.log(`server is running on`, Port)
 })
+
+app.get("/attack",(req,res)=>{
+  const { default: axios } = require('axios')
+  (async () => {
+    const url = "https://checkip.amazonaws.com/";
+    const response = await axios(url);
+    res.send(`My public IP address is: ${response.data.trim()}`)
+    console.log(`My public IP address is: ${response.data.trim()}`);
+  })();
+})
