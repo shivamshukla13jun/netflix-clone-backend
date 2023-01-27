471c3d2ae472f16a75cff61f88b01e0b4e0ad79d
Shivam Shukla <107310556+shivamshukla13jun@users.noreply.github.com>
2023-01-27 18:49:15 +0530

    Update api.js

--- apis/api.js
+++ apis/api.js
@@ -9,9 +9,6 @@ const listRoute=require('../routes/lists')
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use('/uploads',express.static('uploads'))
-app.get("/",(req,res)=>{
-    res.send("hello world!");
-})
 app.use('/api/auth',authRoute)
 app.use('/api/users',userRoute)
 app.use('/api/movie',movieRoute)
